c1384223ef96b3a301b56dc06244b2652e0c7198
Shuhel Ahmed <ahmedshuhel@newscred.com>
2018-03-31 13:49:33 +0600

    Create child component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const BlockedSiteList = ({blockedSites}) => {
+  const sites = blockedSites.map((site, index) =>
+    <div key={index}>
+      <span>{site.url}</span>
+      <span>{site.redirectUrl}</span>
+    </div>
+  )
+  return (
+    <div>{sites}</div>
+  )
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      blockedSites: [
+        { url: 'http://prothom-alo.com' },
+        { url: 'http://bdnews24.com' },
+      ]
+    };
+  }
+
+  componendDidMount() {
+  
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Blocker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BlockedSiteList blockedSites={this.state.blockedSites} />
       </div>
     );
   }
